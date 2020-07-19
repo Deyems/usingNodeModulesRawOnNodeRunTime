a80bf83e0124d91e91efada3356f6f8ee787e201
decagon <decagon@decagons-MacBook-Air.local>
2020-07-19 21:41:38 +0100

    working with node modules that connect with the OS

---
+++ NodeOnOS/index.js
@@ -0,0 +1,13 @@
+const os = require('os');
+
+//We can use various methods on the os like:
+
+/*
+os.cpus();
+os.networkInterfaces().en0.map(i => i.address)
+os.freemem();
+os.type();
+os.release();
+os.userInfo();
+os.constants.signals;
+*/
