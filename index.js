4d4c368d2e66d4378c04486954a1ec8349c0683c
decagon <decagon@decagons-MacBook-Air.local>
2020-07-19 21:32:33 +0100

    adding script to call or use other modules exported in other js files

---
+++ index.js
@@ -0,0 +1,33 @@
+// console.log(arguments);
+
+// process.env.PORT
+// const { config } = require('./util');
+// config.port
+
+// const data = require("data");
+// console.log(data);
+
+// const config = require('./config.json');
+// console.log(config);
+
+// const addon = require('addon');
+// console.log(addon.hello());
+
+/**Overriding REQUIRE fxn */
+// require = function () {
+//     return {mocked: true};
+// }
+// const fs = require('fs');
+// console.log(fs);
+
+// const printStars = require('./printStars');
+// printStars(10, 'Hi!');
+
+// require('./newMod/ascii-art')();
+// console.log(require.cache);
+// delete require.cache['/Users/decagon/Documents/NodeLearningPoint/newMod/ascii-art.js'];
+
+// require('./newMod/ascii-art')();
+
+const express = require('express');
+console.log(express);
