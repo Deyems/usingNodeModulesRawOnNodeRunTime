7aa66a7f569829943dcbd3438b0be391c0fd0bdd
decagon <decagon@decagons-MacBook-Air.local>
2020-07-22 00:41:07 +0100

    adding how to link files through the Os using the child_process module - the child script

---
+++ ChildProcesses/child.js
@@ -0,0 +1,11 @@
+
+// process.on('message', (msg) => {
+//     console.log(`Message from parent:`, msg);
+// });
+
+// let counter = 0;
+
+// setInterval(() => {
+//     process.send({counter: counter++});
+// }, 1000);
+
