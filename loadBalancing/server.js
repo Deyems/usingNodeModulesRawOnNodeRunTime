cc6119197ccadd9ea31c5e980348a4a7795ebe3f
decagon <decagon@decagons-MacBook-Air.local>
2020-07-22 22:52:58 +0100

    a simple server on node

---
+++ loadBalancing/server.js
@@ -0,0 +1,18 @@
+const http = require('http');
+const { worker } = require('cluster');
+
+const pid = process.pid;
+let usersCount;
+
+http.createServer((req, res) => {
+    for(let i = 0; i < 1e7; i++);
+    res.write(`Handled by process ${pid}\n`);
+    res.end(`Users: ${usersCount}`);
+}).listen(8080, () => {
+    console.log(`Started process ${pid}`);
+});
+
+process.on('message', msg => {
+    usersCount = msg.usersCount;
+    
+});
